7e2d56fd0011240d4847400dc14d7cad775fd873
CaoYiMing0 <1099615916@qq.com>
2020-07-10 12:56:29 +0800

    第一次更新

--- day_06/Test05_可变参数.py
+++ day_06/Test05_可变参数.py
@@ -42,3 +42,32 @@ personinfo('小萌', 21, 'beijing', 123, 'jiaozuo', 'happy')
 """
 在函数内部，参数前的星号将所有值放在同一个元组中，通过这种方式收集起来，然后使用。
 """
+
+
+# 关于可变参数的补充
+# a,b为必选参数；c为默认参数；args为可变参数；kw为关键字参数
+def f1(a, b, c=0, *args, **kw):
+    print('a=', a, 'b=', b, 'c=', c, 'args=', args, 'kw=', kw)
+
+
+# 参数定义的顺序必须是：必选参数、默认参数、可选参数、命名关键字参数、关键字参数
+def f2(a, b, c=0, *, d, **kw):
+    print('a=', a, 'b=', b, 'c=', c, 'd=', d, 'kw=', kw)
+
+
+f1(1, 2)
+f1(1, 2, 3)
+f1(1, 2, c=3)
+f1(1, 2, 3, 'a', 'b')
+f1(1, 2, 3, 'a', 'b', x=99, y=88, z=77, t='100')  # 重点***********
+f2(1, 2, city='Shanghai', ext=None)
+"""
+调用结果：
+a= 1 b= 2 c= 0 args= () kw= {}
+a= 1 b= 2 c= 3 args= () kw= {}
+a= 1 b= 2 c= 3 args= () kw= {}
+a= 1 b= 2 c= 3 args= ('a', 'b') kw= {}
+a= 1 b= 2 c= 3 args= ('a', 'b') kw= {'x': 99, 'y': 88, 'z': 77, 't': '100'}
+f2(1, 2, city='Shanghai', ext=None)语句出现如下错误：
+TypeError: f2() missing 1 required keyword-only argument: 'd'
+"""
